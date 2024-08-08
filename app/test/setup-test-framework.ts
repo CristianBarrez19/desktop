847dbc03549b4c9025276cd9807709e9d979026f
CristianBarrez19 <cristianluis19@gmail.com>
2024-08-07 21:23:05 -0300

    Revert "Merge pull request #19060 from robertcrockett/development"
    This reverts commit 6fb905a76c47f2c7751a0a82d7a75e2ab9f56d88, reversing
    changes made to c7544020a23ef8cf2d1e24a376d028916d9f5839.

--- app/test/setup-test-framework.ts
+++ app/test/setup-test-framework.ts
@@ -1,8 +1,7 @@
 // set test timeout to 10s
 jest.setTimeout(10000)
 
-import * as matchers from 'jest-extended'
-expect.extend(matchers)
+import 'jest-extended'
 
 delete process.env['LOCAL_GIT_DIRECTORY']
 delete process.env['GIT_EXEC_PATH']
